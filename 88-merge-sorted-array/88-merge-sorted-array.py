272f972b68e1118586a36838aac6a013a1d411d3
Iana Lysova <lysovayana@mail.ru>
2022-04-22 21:44:20 +0300

    Time: 55 ms (41.28%), Space: 13.7 MB (99.37%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.py
+++ 88-merge-sorted-array/88-merge-sorted-array.py
@@ -4,4 +4,8 @@ class Solution:
         Do not return anything, modify nums1 in-place instead.
         """
         temp1 = nums1[:m]
-        nums1[:] = sorted(temp1+nums2)
+        nums1.clear()
+        temp2 = nums2[:n]
+        temp1.extend(temp2)
+        temp1.sort()
+        nums1.extend(temp1)
