7d2cb2242180b9181080f116f66ba2885fc9021c
Iana Lysova <lysovayana@mail.ru>
2022-04-23 11:31:21 +0300

    Time: 43 ms (70.16%), Space: 14 MB (40.22%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.py
+++ 88-merge-sorted-array/88-merge-sorted-array.py
@@ -3,9 +3,30 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        temp1 = nums1[:m]
-        nums1.clear()
-        temp2 = nums2[:n]
-        temp1.extend(temp2)
-        temp1.sort()
-        nums1.extend(temp1)
+        if m+n == m:
+            return None
+        if m+n == 0:
+            return None
+        
+        mpointer, npointer = m-1, n-1
+        pos = m+n-1
+        
+        while mpointer >= 0 and npointer >= 0:
+            if nums1[mpointer] > nums2[npointer]:
+                nums1[pos] = nums1[mpointer]
+                pos -= 1
+                mpointer -= 1
+            else:
+                nums1[pos] = nums2[npointer]
+                pos -= 1
+                npointer -= 1
+        
+        while npointer >= 0:
+            nums1[pos] = nums2[npointer]
+            pos -= 1
+            npointer -= 1
+        
+        while mpointer >= 0:
+            nums1[pos] = nums1[mpointer]
+            pos -= 1
+            mpointer -= 1
