9867dc5f3e58bcc8b6216907fe6673d27014c424
Iana L <lysovayana@mail.ru>
2022-06-08 15:41:46 +0300

    Delete 26-remove-duplicates-from-sorted-array directory

--- 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def removeDuplicates(self, nums: List[int]) -> int:
-        k = 0
-        for i in range(len(nums)):
-            if i == len(nums)-1 or nums[i] != nums[i+1]:
-                nums[k] = nums[i]
-                k += 1
-        return k
