256d03624b020c182a22b89892613d760d959916
Iana L <lysovayana@mail.ru>
2022-06-09 09:48:21 +0300

    Time: 60 ms (11.84%), Space: 13.9 MB (73.96%) - LeetHub

--- 100-same-tree/100-same-tree.py
+++ 100-same-tree/100-same-tree.py
@@ -6,9 +6,10 @@
 #         self.right = right
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        if not p and not q:
+        if not p and not q: #если нет узлов, деревья равны
             return True
-        if p and q and p.val == q.val:
+        if p and q and p.val == q.val: #проверка существования узлов и их равенство
+            #в стеке функция с левыми нодами, затем функция с правыми
             return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
         return False
         
