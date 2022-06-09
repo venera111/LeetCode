c5fdafb63ee29baa81f96d24e3e4e9b507cdf65b
Iana L <lysovayana@mail.ru>
2022-06-09 09:48:57 +0300

    Time: 31 ms (89.20%), Space: 14 MB (28.42%) - LeetHub

--- 100-same-tree/100-same-tree.py
+++ 100-same-tree/100-same-tree.py
@@ -9,7 +9,7 @@ class Solution:
         if not p and not q: #если нет узлов, деревья равны
             return True
         if p and q and p.val == q.val: #проверка существования узлов и их равенство
-            #в стеке функция с левыми нодами, затем функция с правыми
+            #на стеке функция с левыми нодами, затем функция с правыми
             return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
         return False
         
